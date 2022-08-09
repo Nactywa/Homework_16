1cceb845c07036b68de71996907502014ee6f3e3
Nastya Zbukareva <nactywa@mail.ru>
2022-08-09 15:06:27 +0300

    Ready

--- Homework_16.cpp
+++ Homework_16.cpp
@@ -1,10 +1,50 @@
 ﻿// Homework_16.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <time.h>
+#include <iomanip>
+#include <ctime>
+#include <Windows.h>
+
+
+int GetDay()
+{
+    time_t t;
+
+    time(&t);
+
+    return localtime(&t)->tm_mday;
+}
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    const int size = 5;
+    int G = 0;
+    int F = GetDay() % size;
+    int array[size][size];
+    int Line;
+    
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << "Line_" << i << ":";
+        
+        for (int j = 0; j < size; j++)
+        {
+           
+            array[i][j] = i + j;
+            std::cout << array[i][j] << "";
+            if (F == i)
+            {
+                Line = i;
+                G += array[i][j];
+            }
+        }
+        std::cout << "\n";
+    }
+    std::cout << "--------\n" << "\nDay: " << GetDay() << "\nCurrent Line: " << Line << "\nG: " << G;
 }
 
